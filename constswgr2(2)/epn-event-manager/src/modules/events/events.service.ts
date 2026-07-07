71022a627852c3cd8dd39ecbc407510d60401ab8
fernygeek <yofer68@gmail.com>
2026-07-07 13:07:52 -0500

    corrige(event-manager): rechazar acciones inválidas con HTTP 400
    Refs #13

--- constswgr2(2)/epn-event-manager/src/modules/events/events.service.ts
+++ constswgr2(2)/epn-event-manager/src/modules/events/events.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { CreateEventDto } from './dto/create-event.dto';
@@ -82,7 +82,7 @@ export class EventsService {
       return { ok: true };
     }
 
-    return { ok: false };
+    throw new BadRequestException('Acción no válida');
   }
 
   async findAll(): Promise<object[]> {
